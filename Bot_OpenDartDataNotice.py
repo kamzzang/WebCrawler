f09c080f750b9cc27c3d0d5ceb74dc0e9b9bba11
Moochul Kam <kamzzang1@naver.com>
2020-12-05 22:41:45 +0900

    Add : Telegram Function

--- Bot_OpenDartDataNotice.py
+++ Bot_OpenDartDataNotice.py
@@ -3,6 +3,19 @@ from urllib.request import urlopen
 import re
 import sqlite3
 import pandas as pd
+import json
+import telepot
+import os
+
+admin_info_file = os.getenv('APPLICATION_ADMIN_INFO') # 환경 변수에 저장한 중요 개인 정보 불러옴
+with open(admin_info_file, 'r') as f:
+    admin_info = json.load(f)
+
+token = admin_info['telegram']['token'] # "Individual Telegram Token"
+Bot_ID = admin_info['telegram']['id']['mc'] # "Receiver's Telegram ID"
+# 결과 텔레그램 전송
+bot = telepot.Bot(token)
+
 
 def Search():
     url = "http://dart.fss.or.kr/dsac001/mainAll.do"
@@ -19,7 +32,9 @@ def Search():
         report = re.sub(r'[\t\n\r ]', '', tr.findAll('td')[2].find('a').text)  # 보고서명
         link = 'dart.fss.or.kr' + tr.findAll('td')[2].find('a').attrs['href']  # url
 
-        print('[기업공시알림]\n%s\t%s_%s\n%s\n%s\n' % (company,  date, time, report, link))
+        msg = '[기업공시알림]\n%s\t%s_%s\n%s\n%s\n' % (company,  date, time, report, link)
+        print(msg)
+        bot.sendMessage(Bot_ID, msg)
 
 if __name__ == '__main__':
     Search()
