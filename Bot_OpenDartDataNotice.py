776de27d4dbd9af2f957d240736c0ffe12306992
Moochul Kam <kamzzang1@naver.com>
2020-12-08 23:37:04 +0900

    Add : comment

--- Bot_OpenDartDataNotice.py
+++ Bot_OpenDartDataNotice.py
@@ -1,8 +1,8 @@
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import datetime
 import pandas as pd
+import pandas.io.sql as pd_sql
 import json
 import telepot
 import sqlite3
@@ -63,25 +63,40 @@ def Check_Dart(stock):
         df = pd.read_sql("SELECT 보고서번호 from DART WHERE 종목명='%s'"%(stock), con=con)
         con.close()
 
-        report_num = df['보고서번호'].tolist()
+        report_num = df['보고서번호'].tolist() # 저장된 공시의 보고서 번호만 리스트로 반환
 
         return report_num
-    except Exception as e:
+    except Exception as e: # DART 테이블이 없거나 에러발생하면 빈 리스트를 반환
         print('Check_Dart Error : %s' %e)
         return []
 
+# 신규 공시 DB저장
+def DatatoSQL(df):
+
+    con = sqlite3.connect("OpenDartBot.db")
+
+    company = df['종목명'][0]
+    sql = "DELETE FROM DART WHERE 종목명='%s'" % (company) # 해당 기업 데이터 삭제
+    pd_sql.execute(sql, con)
+    con.commit()
+
+    df.to_sql('DART', con, if_exists='append', index=False) # 신규 데이터 저장
+    con.close()
+
 # API를 통한 공시 정보 수집
 def Search_Dart(today, stock_list):
+    # 금일 공시 정보만 받기 위해 마지막 날짜 변수 저장
     end_date = today.strftime("%Y%m%d")
 
+    # 고유번호 dataframe
     con = sqlite3.connect("DB.db")
     df_corp = pd.read_sql('SELECT * from LIST', con=con)
     con.close()
 
-    for stock in stock_list:
-        new_msg = False
+    for stock in stock_list: # 관심 종목 list 하나씩 처리
+        new_msg = False # 초기에 False 로 선언
 
-        corp_code = df_corp[df_corp['종목명'] == stock]['고유번호'].values[0]
+        corp_code = df_corp[df_corp['종목명'] == stock]['고유번호'].values[0] # 회사명으로 고유번호 받아옴
         url = "https://opendart.fss.or.kr/api/list.json?crtfc_key=%s&corp_code=%s&end_de=%s&page_count=100" % (
             crtfc_key, corp_code, end_date)
 
@@ -107,7 +122,7 @@ def Search_Dart(today, stock_list):
             report_num = Check_Dart(stock)
 
             for num in df['보고서번호'].tolist():
-                if num not in report_num:
+                if num not in report_num: # 신규 정보 확인
                     new_msg = True
 
                     report = df[df['보고서번호'] == num]['보고서명'].values[0]
@@ -116,12 +131,10 @@ def Search_Dart(today, stock_list):
 
                     print('[기업공시알림]\n%s\n%s\n제출 : %s\n%s' % (stock, report, name, link))
                     msg = '[기업공시알림]\n%s\n%s\n제출 : %s\n%s' % (stock, report, name, link)
-                    Sendmsg(msg)
+                    Sendmsg(msg) # 텔레그램 메세지 전송
 
             if new_msg == True:
-                con = sqlite3.connect("DB.db")
-                df.to_sql('DART', con, if_exists='append', index=False)
-                con.close()
+                DatatoSQL(df)
             else:
                 print('%s : 신규 공시 없음' % (stock))
 
@@ -129,6 +142,6 @@ def Search_Dart(today, stock_list):
             print('%s : 데이터없음' % stock)
 
 if __name__ == '__main__':
-    today = datetime.date.today()
-    stock_list = ['삼성전자', 'SK하이닉스', '한화솔루션', 'LG화학']
-    Search_Dart(today, stock_list)
+    today = datetime.date.today() # 오늘 날짜 저장
+    stock_list = ['삼성전자', 'SK하이닉스', '한화솔루션', 'LG화학'] # 원하는 종목을 리스트로 만듬
+    Search_Dart(today, stock_list) # 공시 정보 수집 시작
